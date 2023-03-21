36fbea638a6ea2f705799a9b816eb084174cd2fb
jueun <juandeun4136@gmail.com>
2023-03-21 20:05:53 +0900

    - remove hello.java - add MenuManager

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+	public static void main(String[] args) {
+		System.out.println("1. Add Studnets");
+		System.out.println("2. Delete Studnets");
+		System.out.println("3. Edit Studnets");
+		System.out.println("4. View Studnets");
+		System.out.println("5. Exit");
+		
+	}
+}
+ 
